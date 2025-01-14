0d2089208e506f9a9e224dfafaf8f74f9bf6318c
Dananjaya M Perera <dan@macoocoo.com>
2025-01-15 10:06:06 +1300

    Added database Schema and Seeding

---
+++ impactlab-test-app/database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'description' => fake()->sentence(),
+            'quantity' => random_int(0, 100),
+            'price' => fake()->randomFloat(2, 0, 100),
+        ];
+    }
+}
